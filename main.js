92941f42bf364cd5c92d00ac9fb305c6c6d838eb
Rhys Evans <rhys.evans@ft.com>
2014-07-24 10:32:28 +0100

    added javascript helper to find current layer context

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var dom = require('o-dom');
+
+module.exports = {
+    getLayerContext: function (el) {
+        return dom.getClosestMatch(el, '.o-layers__context') || document.body;
+    }
+};
