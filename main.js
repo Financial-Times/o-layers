cda2fab40cd5a92ada43d728f52fe297aaeaf500
Alberto Elias <me@alberto-elias.com>
2015-01-08 13:21:27 +0000

    Deprecated layers context class in lieu of a data attribute

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var dom = require('o-dom');
 
 module.exports = {
     getLayerContext: function (el) {
-        return dom.getClosestMatch(el, '.o-layers__context') || document.body;
+        return dom.getClosestMatch(el, '[data-o-layers-context]') || document.body;
     }
 };
